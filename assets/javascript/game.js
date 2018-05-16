ab5e71bf148f0ed871fd8629335eadb10f9ba379
jfenneyj <jfenney@leadtomarket.com>
2018-05-15 18:30:28 -0700

    update files1

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//random number at the start of the game between 19 - 120.
+// Each crystal has a random number between 1 - 12.
+// When the user clicks on a crystal it add a specific amount of points to user total score.
+// the random number will be hidden until crystal is clicked
+// update the user score when crystal is clicked
+// User wins when users score is equal to beginning random number.
+// User loses if users total is greater then randomNumber.
